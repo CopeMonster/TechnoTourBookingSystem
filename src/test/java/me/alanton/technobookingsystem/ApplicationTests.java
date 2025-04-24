0837c5c846c7c3162e9389b8da76cef19a35c4d4
alankasymbek <alankassymbek@gmail.com>
2025-04-24 16:17:05 +0500

    git init

---
+++ src/test/java/me/alanton/technobookingsystem/ApplicationTests.java
@@ -0,0 +1,12 @@
+package me.alanton.technobookingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
